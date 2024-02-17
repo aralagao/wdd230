d6c6a9cfe819ca4c95636edf0bb724ae9cbd2988
aralagao <arthuralagao@gmail.com>
2024-02-16 23:13:57 -0800

    changes to chamber page

---
+++ chamber/scrtipts/windchill.js
@@ -0,0 +1,8 @@
+function calculateWindChill(temperature, windSpeed) {
+  if (temperature <= 50 && windSpeed > 3.0) {
+      var windChill = 35.74 + 0.6215 * temperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
+      return Math.round(windChill);
+  } else {
+      return "N/A";
+  }
+}
