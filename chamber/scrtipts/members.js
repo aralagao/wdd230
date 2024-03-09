0100bf41a7481a3eedc5c2116c4bc43fa15b8167
aralagao <arthuralagao@gmail.com>
2024-03-08 22:44:46 -0800

    trial

--- chamber/scrtipts/members.js
+++ chamber/scrtipts/members.js
@@ -1,34 +1,35 @@
-const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const url = 'https://aralagao.github.io/wdd230/chamber/data/members.json';
 const cards = document.querySelector('#cards');
-async function getProphetData() {
+async function getMemberData() {
   const response = await fetch(url);
   const data = await response.json();
-  displayProphets(data.prophets);
+  displayMembers(data.members);
 
 }
 
-getProphetData();
+getMemberData();
 
-const displayProphets = (prophets) => {
-  prophets.forEach((prophet) => {
+const displayMembers = (members) => {
+  members.forEach((member) => {
     let card = document.createElement('section');
-    let fullName = document.createElement('h2');
-    let birthdate = document.createElement('p');
-    let birthplace = document.createElement('p');
+    let name = document.createElement('h2');
+    let address = document.createElement('p');
+    let phoneNumber = document.createElement('p');
     let portrait = document.createElement('img');
-    fullName.textContent = `${prophet.name} ${prophet.lastname}`;
-    birthdate.textContent = `Date of Birth: ${prophet.birthdate}`;
-    birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
+    
+    name.textContent = `${member.name}`;
+    address.textContent = `Address: ${member.address}`;
+    phoneNumber.textContent = `Phone Number: ${member.phoneNumber}`;
 
-    portrait.setAttribute('src', prophet.imageurl);
-    portrait.setAttribute('alt', `Portrait of ${prophet.firstName} ${prophet.lastName}`);
+    portrait.setAttribute('src', member.imageurl);
+    portrait.setAttribute('alt', `Portrait of ${member.name}`);
     portrait.setAttribute('loading', 'lazy');
     portrait.setAttribute('width', '340');
     portrait.setAttribute('height', '440');
 
-    card.appendChild(fullName);
-    card.appendChild(birthdate);
-    card.appendChild(birthplace);
+    card.appendChild(name);
+    card.appendChild(address);
+    card.appendChild(phoneNumber);
     card.appendChild(portrait);
 
     cards.appendChild(card);
